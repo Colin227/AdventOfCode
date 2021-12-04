26da39d6b849e7c8672533afae1beb255d279b03
Colin <colin9416@gmail.com>
2021-12-04 15:32:08 -0500

    Refactored to remove unneccesary function

--- Day3/day3-1.py
+++ Day3/day3-1.py
@@ -8,30 +8,29 @@ instructions = [instruction.rstrip() for instruction in instructions]
 def processInput():
     countOne = 0
     countZero = 0
-
-
     mostCommon = []
     leastCommon = []
+
+
     for i in range(0, 12): # loop through each digit in instruction
         for instruction in instructions: #loop through each instruction[i]
-            tmpCount = countValue(instruction[i])
-            if tmpCount > 0:
+            if int(instruction[i]): # If value is 1 this evaluates as true
                 countOne += 1  
             else:
                 countZero += 1
-        if countOne > countZero:
+                
+        # Compare number of 0's and 1's, append most/least common value to appropriate array
+        if countOne > countZero: 
                 mostCommon.append(1)
                 leastCommon.append(0)
         else:
                 mostCommon.append(0)
                 leastCommon.append(1)
+        
+        # reset counter before moving to next index
         countOne = 0
         countZero = 0
         #print(countOne, countZero)
     print(mostCommon, leastCommon)
 
-def countValue(currentValue):
-    if int(currentValue) == 1:
-        return 1
-    else: return 0
 processInput()
